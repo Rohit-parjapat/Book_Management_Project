d9a6127e53ba863d3bb1837ec7e125634234d723
Rohit-parjapat <rparjapati7377@gmail.com>
2023-01-29 17:03:26 +0530

    removed redundancy

--- Models/users-model.js
+++ Models/users-model.js
@@ -1,48 +1,46 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     name: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     surname: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     issuedBook: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Book",
-        required: false,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Book",
+      required: false,
     },
     issuedDate: {
-        type: String,
-        required: false,
+      type: String,
+      required: false,
     },
     returnDate: {
-        type: String,
-        required: false,
-    },
-    returnDate: {
-        type: String,
-        required: false,
+      type: String,
+      required: false,
     },
     subscriptionType: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     subscriptionDate: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-},
-    {
-        timestamps: true,
-    });
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
